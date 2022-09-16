648c6c469aa123d1de47c1ec0057ab00405171d3
Andrew Beard <bearda@gmail.com>
2022-09-15 20:25:01 -0400

    - Add a BCD output mode - Function type hints

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from microbit import *
+from microbit import accelerometer, button_a, button_b
 import random
+
+from output import ScrollingOutputHandler, BCDOutputHandler
 
 DIE_FACES = [3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 20, 24, 30, 100]
 
 
-def roll_result(faces):
+def roll_result(faces: int) -> int:
     return random.randint(1, faces)
 
+
+if button_a.is_pressed():
+    output = BCDOutputHandler()
+else:
+    output = ScrollingOutputHandler()
 
 die_index = DIE_FACES.index(20)
 while True:
@@ -14,11 +21,11 @@ while True:
     if a_presses > 0:
         # Switch die size
         die_index = (die_index + a_presses) % len(DIE_FACES)
-        display.scroll("d{}".format(DIE_FACES[die_index]), delay=100)
+        output.die(DIE_FACES[die_index])
     elif button_b.was_pressed() or accelerometer.was_gesture("shake"):
         # Wait for any shaking to end first
         while accelerometer.is_gesture("shake"):
-            display.show("?")
+            output.shaking()
         # Roll the die
         result = roll_result(DIE_FACES[die_index])
-        display.scroll(str(result))
+        output.roll(result)
---
+++ output.py
@@ -0,0 +1,43 @@
+class OutputHandler:
+    def roll(self, result: int) -> None:
+        pass
+
+    def shaking(self) -> None:
+        pass
+
+    def die(self, faces: int) -> None:
+        pass
+
+
+class ScrollingOutputHandler(OutputHandler):
+    def roll(self, result: int) -> None:
+        display.scroll(result)
+
+    def shaking(self) -> None:
+        display.show("?")
+
+    def die(self, faces: int) -> None:
+        display.scroll("d{}".format(faces), delay=100)
+
+
+class BCDOutputHandler(OutputHandler):
+    brightness = 6
+
+    def __init__(self) -> None:
+        display.clear()
+
+    def show_column_value(self, column: int, value: int) -> None:
+        for i in range(5):
+            if int(value) & (1 << i):
+                disp = self.brightness
+            else:
+                disp = 0
+            display.set_pixel(column, 4 - i, disp)
+
+    def roll(self, result: int) -> None:
+        self.show_column_value(0, result / 10)
+        self.show_column_value(1, result % 10)
+
+    def die(self, faces: int) -> None:
+        self.show_column_value(3, faces / 10)
+        self.show_column_value(4, faces % 10)
